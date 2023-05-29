c011d2e58ad2ab79a67c502dc5aab8f5f507920a
sushil <sushilbajpai2003@gmail.com>
2023-05-29 18:47:05 +0530

    adfass

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/global.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
