bc9db252468a273dd9e6e52bdbb0f37a45218f8a
sushil <sushilbajpai2003@gmail.com>
2023-06-04 00:05:41 +0530

    sdsd

--- frontend/src/network/notes_api.ts
+++ frontend/src/network/notes_api.ts
@@ -4,28 +4,37 @@ import { User } from "../models/user";
 import axios from 'axios';
 
 async function fetchData(input: RequestInfo, init?:RequestInit){
-    const response=await fetch(input, init);
-    if(response.ok){
-        return response;
-    }
-    else{
-        const errorBody=await response.json();
-        const errorMessage=errorBody.error;
-        if(response.status===401){
-            throw new UnauthorizedError(errorMessage);
-        }
-        else if(response.status===409){
-            throw new ConflictError(errorMessage);
+    try {
+        const response=await fetch(input, init);
+        if(response.ok){
+            return response;
         }
         else{
-            throw Error("Request failed with status: "+response.status+" message: "+errorMessage);
+            const errorBody=await response.json();
+            const errorMessage=errorBody.error;
+            if(response.status===401){
+                throw new UnauthorizedError(errorMessage);
+            }
+            else if(response.status===409){
+                throw new ConflictError(errorMessage);
+            }
+            else{
+                throw Error("Request failed with status: "+response.status+" message: "+errorMessage);
+            }
         }
+    } catch (error) {
+        
     }
+   
 }
 
 export async function getLoggedInUser(): Promise<User>{
-    const response=await fetchData("/api/notes", {method: "GET"});
-    return response.json();
+    const response=await axios.request({
+        url: "/api/notes",
+        baseURL: "https://notes-app-backendn.onrender.com",
+        method: "GET",
+    })
+    return response?.data;
 }
 export interface signUpCredentials{
     username: string,
@@ -34,15 +43,17 @@ export interface signUpCredentials{
 }
 
 export async function signUp(credentials: signUpCredentials): Promise<User>{
-    const response=await fetchData("/api/users/signup", {
-        method: "POST",
-        
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(credentials)
+    const response=await axios.request({url: "/api/users/signup",
+    baseURL: "https://notes-app-backendn.onrender.com",
+    method: "POST",
+    withCredentials: true,
+    headers: {
+        "Content-Type": "application/json",
+    },
+    data: credentials
+       
     });
-    return response.json();
+    return response?.data;
 }
 export interface loginCredentials{
     username: string,
@@ -62,18 +73,26 @@ export async function login(credentials: any){
         data: credentials
     });
     alert(JSON.stringify(response?.data));
-    return response;
+    return response?.data;
 }catch(err: any){
     alert('login err =>'+ err?.response?.data + "\n" + err?.message);
 }
 }
 export async function logout(){
-    await fetchData("/api/users/logout", {method: "POST"});
+    await axios.request({
+        url: "/api/users/logout",
+        baseURL: "https://notes-app-backendn.onrender.com",
+        method: "POST",
+    })
 }
 
 export async function fetchNotes(): Promise<Note[]>{
-    const response=await fetchData("/api/notes", {method: "GET"});
-    return response.json();
+    const response=await axios.request({
+        url: "/api/notes",
+        baseURL: "https://notes-app-backendn.onrender.com",
+        method: "GET",
+    });
+    return response?.data;
 }
 
 export interface NoteInput{
@@ -81,29 +100,32 @@ export interface NoteInput{
     text?: string,
 }
 
-export async function createNote(note: NoteInput): Promise<Note>{
-    const response=await fetchData("/api/notes",
-    {
-        method: "POST",
-        
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(note),
-    })
-    return response.json();
+export async function createNote(note: any): Promise<Note>{
+    const response=await axios.request({
+        url: "/api/notes",
+        baseURL: "https://notes-app-backendn.onrender.com",
+            method: "POST",
+            
+            headers: {
+                "Content-Type": "application/json",
+            },
+            data: note,
+        })
+    return response?.data;
 }
 
 export async function updateNote(noteId: string, note: NoteInput): Promise<Note>{
-    const response=await fetchData("/api/notes/"+noteId, {
+    const response=await axios.request({
+        url: "/api/notes/"+noteId,
+        baseURL: "https://notes-app-backendn.onrender.com",
         method: "PATCH",
         
         headers: {
             "Content-Type": "application/json",
         },
-        body: JSON.stringify(note),
+        data: note,
     });
-    return response.json();
+    return response?.data;
 }
 
 export async function deleteNote(noteId: string){
